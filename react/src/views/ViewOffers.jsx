f77a1c53d3230b324e77d92a74e7ee3c93f2cc7d
Poori99 <poornimajayathilake1999@gmail.com>
2023-09-24 14:36:32 +0530

    modify headers

--- react/src/views/ViewOffers.jsx
+++ react/src/views/ViewOffers.jsx
@@ -279,7 +279,7 @@ Remove
                 <div>
                     <header className="bg-white shadow">
                         <div className="flex mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                            <h1 className="text-3xl font-bold tracking-tight text-gray-900 mt-20">Offers</h1>
+                            <h1 className="text-3xl font-bold tracking-tight text-gray-900">Offers</h1>
                             <div className="loading-container">
                                 {/* {loading && <p className="loading-text">Loading...</p>} */}
                             </div>
