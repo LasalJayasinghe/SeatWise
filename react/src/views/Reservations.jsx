3b824db8e1e972963917ed4999605405fcee9397
Poori99 <poornimajayathilake1999@gmail.com>
2023-09-24 12:46:39 +0530

    minor changes

--- react/src/views/Reservations.jsx
+++ react/src/views/Reservations.jsx
@@ -96,7 +96,7 @@ export default function Reservations() {
     <>
       <header className="bg-white shadow"> 
         <div className="flex mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-          <h1 className="text-3xl font-bold tracking-tight text-gray-900 mt-20">Reservations</h1>
+          <h1 className="text-3xl font-bold tracking-tight text-gray-900">Reservations</h1>
           <div className="loading-container">
             {loading && <p className="loading-text">Loading...</p>}
           </div>
