0a7b899923781847b35d35e53330c82635fac002
Lahiru Kavishka <117619338+Lahiru2204@users.noreply.github.com>
2023-10-30 14:52:14 +0530

    system controller created

---
+++ app/Http/Controllers/Api/SystemController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Carbon\Carbon; // Import Carbon for working with dates and times
+
+class SystemController extends Controller{
+
+    public function systemDashboard(){
+        
+
+    }
+
+
+
+}
