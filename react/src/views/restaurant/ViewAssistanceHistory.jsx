d4f0305bc70b74727392ae360d85c1ef10a4355f
Madushi Weerawarna <wmadushi49@gmail.com>
2023-10-31 10:06:12 +0530

    fixing headers

--- react/src/views/restaurant/ViewAssistanceHistory.jsx
+++ react/src/views/restaurant/ViewAssistanceHistory.jsx
@@ -6,6 +6,7 @@ import { useNavigate } from "react-router-dom";
 import DeleteConfirmationModal from "../../components/restaurant/DeleteConfirmationModal";
 import RequestViewModal from "../../components/restaurant/RequestViewModal";
 import ReplyViewModal from "../../components/restaurant/ReplyViewModal";
+import SettingsBar from "../../components/restaurant/SettingsBar";
 import {
   DataGrid,
   GridToolbar,
@@ -238,16 +239,24 @@ Not Solved
 
   return (
     <>
-      <header className="bg-white shadow">
-        <div className="flex mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-          <h1 className="text-3xl font-bold tracking-tight text-gray-900">Technical Assistance</h1>
-          <div className="loading-container">
-            
-          </div>
-        </div>
-        </header>
-
+    
         <main>
+        <div className="ordercontainer">
+        <div className="menuContainer">
+       
+                <SettingsBar />
+            </div>
+        <div>
+                    <header className="bg-white shadow">
+                        <div className="flex mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                            <h1 className="text-3xl font-bold tracking-tight text-gray-900">Technical Assistance</h1>
+                            <div className="loading-container">
+                                {/* {loading && <p className="loading-text">Loading...</p>} */}
+                            </div>
+                        </div>
+                    </header>
+            
+            <div className="contentContainer">
         <button onClick={handleClick} style={{ marginLeft: '73rem',
      marginTop: '3rem',
      fontSize: '1.3rem', // Increase font size
@@ -305,9 +314,10 @@ Not Solved
                     disableDensitySelector
                     disableColumnSelector
                 />
-
+             </div>
             </div>
-
+            </div>
+          </div>
             </div>
             <RequestViewModal
         isOpen={showViewModal}
