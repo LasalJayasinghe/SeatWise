da6ae889f06187bd4d2ec41e96373df7a411de23
Lahiru Kavishka <117619338+Lahiru2204@users.noreply.github.com>
2023-11-01 08:20:29 +0530

    new functions

---
+++ app/Models/Rate.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Rate extends Model
+{
+    use HasFactory;
+
+    protected $table = 'rate';
+
+    protected $primaryKey = 'rateID';
+
+    protected $fillable = [
+        'starCount',
+        'customerID',
+        
+
+    ];
+
+}
